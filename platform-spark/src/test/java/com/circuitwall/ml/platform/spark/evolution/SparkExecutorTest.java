f37886e304fb58b8ea68a27bd4578c65a43635ea
Andrew Wu <andrew.wu@thinkbiganalytics.com>
2017-08-04 14:41:07 +0200

    removed one test

--- platform-spark/src/test/java/com/circuitwall/ml/platform/spark/evolution/SparkExecutorTest.java
+++ platform-spark/src/test/java/com/circuitwall/ml/platform/spark/evolution/SparkExecutorTest.java
@@ -10,21 +10,9 @@ import org.junit.Test;
 import java.util.Arrays;
 
 public class SparkExecutorTest {
+
     @Test
     public void execute() throws Exception {
-        long start = System.currentTimeMillis();
-        TestAlgorithm simulator = new TestAlgorithm();
-        SparkConf conf = new SparkConf();
-        conf.setMaster("local");
-        JavaSparkContext context = new JavaSparkContext("local", "EA");
-        SparkExecutor executor = new SparkExecutor(context, 10);
-        Comparable[] finalResult = executor.execute(simulator, 100, 5000, 100, 5D);
-        System.out.println("Took:" + (System.currentTimeMillis() - start) + " Final result:" + Arrays.toString(finalResult));
-    }
-
-
-    @Test
-    public void execute2() throws Exception {
 
         long start = System.currentTimeMillis();
         ComplexEquationAlgorithm simulator = new ComplexEquationAlgorithm(8848D, "${val0}+${val1}*${val2}-Math.sqrt(${val3}+${val1})", 0, 100);
